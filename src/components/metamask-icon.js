592bac8ab2b9f3f1f4adf0c95a82df6d64051334
Ilhwan Seo <erani@Ilhwanui-MacBookPro.local>
2022-01-01 16:36:58 +0900

    Added MetaMask Icon Button

--- src/components/metamask-icon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Icon } from '@mui/material/Icon';
-import MetaMaskImg from "../images/metamask.svg"
-
-export const MetaMaskIcon = () => (
-    <Icon>
-        <img src={MetaMaskImg} height={15} width={15}/>
-    </Icon>
-)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 
 import IconButton from '@mui/material/IconButton';
-import MetaMaskIcon from '../components/metamask-icon'
+import SvgIcon from '@mui/material/SvgIcon';
+import { Button, Avatar } from "@mui/material";
 
 
 // styles
@@ -136,21 +137,21 @@ const IndexPage = () => {
         ))}
       </ul> */}
       <p style={paragraphStyles}>
-      Please make sure you are connected to Polygon Mainnet and the correct address. Please note: Once you make the purchase, you cannot undo this action.
+        Please make sure you are connected to Polygon Mainnet and the correct address. Please note: Once you make the purchase, you cannot undo this action.
       </p>
       {/* <button type="button" class="btn btn-xss btn-soft-light text-nowrap d-flex align-items-center mr-2" onclick="addNetwork('web3');"> */}
-        {/* <img width="15" src={MetaMaskImg} alt="Metamask"/> Add Polygon Network */}
+      {/* <img width="15" src={MetaMaskImg} alt="Metamask"/> Add Polygon Network */}
       {/* </button> */}
       {/* <Button variant="contained" color="primary" startIcon={<DeleteIcon />}>
       Hello World
     </Button> */}
-    <IconButton aria-label="delete">
-      <MetaMaskIcon />
-    </IconButton>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<Avatar src={'/images/metamask.svg'} />}
+      >
+        Add to network
+      </Button>
     </main>
   )
 }
